7bf8639e4a0045e2838bcd3047c556cfb611294c
yinnping <ciaoing@livemail.tw>
2018-05-14 19:46:16 +0800

    Add loop and file function. Now it can output a file.

--- crawler.py
+++ crawler.py
@@ -3,28 +3,61 @@ import sys
 import requests
 from bs4 import BeautifulSoup
 
-def GetDetail(url):
+# Store all detail of object.
+gList = []
+
+def GetDetail(url):   
+    # TODO: Error control.
+
+    global gList
+    if url == 'done':
+        f = open('ouput.txt', 'w', encoding='utf8')    
+        for line in gList:
+            f.write(line+ '\n')
+        f.close()
+        return 2
+
     # Needs user-agent to get all context.
     headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36 Edge/17.17134'}
     detail = requests.get(url, headers=headers)
 
     soup = BeautifulSoup(detail.text, 'html.parser')
-    # ss = soup.find_all(attrs={"class": "addr", "name": "hid_address", "id": "hid_imgArr"})
 
     address = soup.find_all(class_="addr")[0]
     title = soup.find_all('input', id="hid_address")[0]
-    # imgs = soup.find_all(id="hid_imgArr")
     price = soup.find_all(class_="price")[0].find('i')
 
-    print(address.text)
-    print(title['value'])
-    print(price.text.split(' ')[0])
-    print(url)
+    gList.append("{}\t{}\t{}\t{}".format(address.text, title['value'], price.text.split(' ')[0], url))
+    return 1
 
-def main():
-    # print(sys.argv[1])
-    url = input("Give me a url: ")
-    GetDetail(url)
+def GetFromFile(filePath):
+    global gList
+    f = open(filePath, 'r+', encoding='utf8')
+    lines = f.readlines()
+
+    # Reset position from EOF to 0
+    f.seek(0)
+
+    for i in range(len(lines)):
+        if i != len(lines) - 1:
+            GetDetail(lines[i][:-1])
+        else:
+            GetDetail(lines[i])
+        f.write(gList[i] + '\n')    
+
+    f.close()
+
+def main():    
+    filePath = ''
+    url = ''
+    if len (sys.argv) != 1:
+        filePath = sys.argv[1]
+        GetFromFile(filePath)        
+    else:
+        r = 1
+        while r == 1:
+            url = input('URL >>> ')
+            r = GetDetail(url)
 
 if __name__ == '__main__':
     main()
